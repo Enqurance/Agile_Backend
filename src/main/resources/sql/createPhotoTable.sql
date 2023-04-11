d997360f16a200d4a104b5d956e3c48f5e1c7675
杜维康 <20373862@buaa.edu.cn>
2023-04-11 12:57:37 +0800

    [feat](): fix name problem

--- src/main/resources/sql/createPhotoTable.sql
+++ src/main/resources/sql/createPhotoTable.sql
@@ -1,6 +1,6 @@
 # 创建photo表的SQL语句
 CREATE TABLE `buaa_map`.`photo` (
     `ph_id` INT NOT NULL AUTO_INCREMENT COMMENT '照片组id，系统自动生成' ,
-    `ph_urls` VARCHAR(2000) NOT NULL COMMENT '照片组url' ,
+    `ph_url` VARCHAR(2000) NOT NULL COMMENT '照片组url' ,
     PRIMARY KEY (`ph_id`)
                           ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
