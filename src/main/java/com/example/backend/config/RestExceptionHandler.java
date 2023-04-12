8be6b110eb83eb629ea1044f9f227e2d109df202
WAN-M <1437207980@qq.com>
2023-04-12 16:45:51 +0800

    [feat](): global exception handler

---
+++ src/main/java/com/example/backend/config/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.backend.config;
+
+import com.example.backend.result.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class RestExceptionHandler {
+    /**
+     * 默认全局异常处理。
+     * @param e the e
+     * @return ResultData
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResult<String> exception(Exception e) {
+        log.error("全局异常信息 ex={}", e.getMessage(), e);
+        return CommonResult.failed(e.getMessage());
+    }
+
+}
