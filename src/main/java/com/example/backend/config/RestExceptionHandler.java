5b747d93f405c662e2185b82905a71aac76a1bb0
WAN-M <1437207980@qq.com>
2023-04-20 20:14:21 +0800

    [feat](): http status is always 200

--- src/main/java/com/example/backend/config/RestExceptionHandler.java
+++ src/main/java/com/example/backend/config/RestExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.example.backend.config;
 
 import com.example.backend.result.CommonResult;
-import com.example.backend.result.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,18 +13,17 @@ public class RestExceptionHandler {
     /**
      * 默认全局异常处理。
      * @param e the e
-     * @return ResultData
+     * @return CommonResult
      */
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public CommonResult<String> exception(Exception e) {
-        // 缺乏相应权限
-        if (e instanceof AccessDeniedException) {
-            return CommonResult.failed(ResultCode.FORBIDDEN);
-        } else {
-            log.error("全局异常信息 ex={}", e.getMessage(), e);
-            return CommonResult.failed(e.getMessage());
-        }
+//        // 缺乏相应权限
+//        if (e instanceof AccessDeniedException) {
+//            return CommonResult.failed(ResultCode.FORBIDDEN);
+//        } else {
+        log.error("全局异常信息 ex={}", e.getMessage(), e);
+        return CommonResult.failed(e.getMessage());
     }
 
 }
