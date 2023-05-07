b503c40b8adea6faae6ce777fabf6cbb469e60a6
WAN-M <1437207980@qq.com>
2023-05-07 09:10:01 +0800

    [fix](): change information put in token

--- src/main/java/com/example/backend/utils/JwtUtil.java
+++ src/main/java/com/example/backend/utils/JwtUtil.java
@@ -38,16 +38,13 @@ public class JwtUtil {
         Map<String, Object> map = new HashMap<>();
         map.put("alg", "HS256");
         map.put("typ", "JWT");
-        String token = JWT.create()
+        return JWT.create()
                 .withHeader(map)// 添加头部
                 //可以将基本信息放到claims中
                 .withClaim("id", user.getId())//userId
-                .withClaim("userName", user.getName())//userName
-                .withClaim("password", user.getPassword())//password
                 .withExpiresAt(expireDate) //超时设置,设置过期的日期
                 .withIssuedAt(new Date()) //签发时间
-                .sign(Algorithm.HMAC256(SECRET)); //SECRET加密
-        return token;
+                .sign(Algorithm.HMAC256(SECRET));
     }
 
     /**
