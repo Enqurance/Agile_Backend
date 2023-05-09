31b59cd916542460a66d96575ae76183ab7efecc
WAN-M <1437207980@qq.com>
2023-05-09 23:27:44 +0800

    [feat](): update TitleGenerator with those new messages

--- src/main/java/com/example/backend/entity/message/TitleGenerator.java
+++ src/main/java/com/example/backend/entity/message/TitleGenerator.java
@@ -20,19 +20,41 @@ public class TitleGenerator {
     public static String generateTitle(Message message) {
         String title;
         switch (message.getType()) {
-            case 1 -> title = LikeTitle(message);
-            case 2 -> title = ReplyTitle(message);
-            case 3 -> title = PinApplySuccessTitle(message);
-            case 4 -> title = PinTaskTitle(message);
-            case 5 -> title = PostReportTitale(message);
-            case 6 -> title = CommentReportTitle(message);
-            case 7 -> title = ExaminePostTitle(message);
+            case 1 -> title = likeTitle(message);
+            case 2 -> title = replyTitle(message);
+            case 3 -> title = pinApplySuccessTitle(message);
+            case 4 -> title = postReleaseSuccessTitle(message);
+            case 5 -> title = pinTaskTitle(message);
+            case 6 -> title = postReportTitle(message);
+            case 7 -> title = floorReportTitle(message);
+            case 8 -> title = commentReportTitle(message);
+            case 9 -> title = examinePostTitle(message);
+            case 10 -> title = pinApplyResultTitle(message);
+            case 11 -> title = postReportResultTitle(message);
+            case 12 -> title = floorReportResultTitle(message);
+            case 13 -> title = commentReportResultTitle(message);
             default -> title = "";
         }
         return title;
     }
 
-    private static String PinApplySuccessTitle(Message message) {
+    private static String likeTitle(Message message) {
+        return MESSTYPE.NOTICE.getType() +
+                userService.findUserById(Integer.parseInt(
+                        message.getPara().split(";")[1]
+                )).get(0).getName() +
+                "点赞了你的帖子"; // TODO 附加帖子标题
+    }
+
+    private static String replyTitle(Message message) {
+        return MESSTYPE.NOTICE.getType() +
+                userService.findUserById(Integer.parseInt(
+                        message.getPara().split(";")[1]
+                )).get(0).getName() +
+                "回复了你";
+    }
+
+    private static String pinApplySuccessTitle(Message message) {
         return MESSTYPE.NOTICE.getType() +
                 "地图钉" +
                 pinService.getPinById(Integer.parseInt(
@@ -41,29 +63,14 @@ public class TitleGenerator {
                 "成功发起公开申请，请等待审核";
     }
 
-    private static String CommentReportTitle(Message message) {
-        return MESSTYPE.REPORT.getType() +
-                "评论" +
-                "" +      // TODO 获取楼层或评论的具体内容
-                "被举报";
-    }
-
-    private static String ExaminePostTitle(Message message) {
-        return MESSTYPE.APPLY.getType() +
-                "帖子" +
-                "" +  // TODO 获取标题
-                "申请发布";
-    }
-
-    private static String LikeTitle(Message message) {
+    private static String postReleaseSuccessTitle(Message message) {
         return MESSTYPE.NOTICE.getType() +
-                userService.findUserById(Integer.parseInt(
-                        message.getPara().split(";")[1]
-                )).get(0).getName() +
-                "点赞了你的帖子"; // TODO 附加帖子标题
+                "您的帖子" +
+                "" +     // TODO 获取帖子标题
+                "发布成功";
     }
 
-    private static String PinTaskTitle(Message message) {
+    private static String pinTaskTitle(Message message) {
         return MESSTYPE.APPLY.getType() +
                 "地图钉" +
                 pinService.getPinById(Integer.parseInt(
@@ -72,18 +79,65 @@ public class TitleGenerator {
                 "申请公开";
     }
 
-    private static String PostReportTitale(Message message) {
+    private static String postReportTitle(Message message) {
         return MESSTYPE.REPORT.getType() +
                 "帖子" +
                 "" +  // TODO 获取帖子标题
                 "被举报";
     }
 
-    private static String ReplyTitle(Message message) {
+    private static String floorReportTitle(Message message) {
+        return MESSTYPE.REPORT.getType() +
+                "楼层" +
+                "" +  // TODO 获取楼层内容
+                "被举报";
+    }
+
+    private static String commentReportTitle(Message message) {
+        return MESSTYPE.REPORT.getType() +
+                "评论" +
+                "" +      // TODO 获取评论的具体内容
+                "被举报";
+    }
+
+    private static String examinePostTitle(Message message) {
+        return MESSTYPE.APPLY.getType() +
+                "帖子" +
+                "" +  // TODO 获取标题
+                "申请发布";
+    }
+
+    private static String pinApplyResultTitle(Message message) {
         return MESSTYPE.NOTICE.getType() +
-                userService.findUserById(Integer.parseInt(
-                        message.getPara().split(";")[1]
-                )).get(0).getName() +
-                "回复了你";
+                "您的地图钉" +
+                pinService.getPinById(Integer.parseInt(
+                        message.getPara()
+                )).getName() +
+                "申请" +
+                message.getPara().split(";")[1];
+    }
+
+    private static String postReportResultTitle(Message message) {
+        return MESSTYPE.NOTICE.getType() +
+                "您对帖子" +
+                "" +   // TODO 获取帖子标题
+                "的举报" +
+                message.getPara().split(";")[1];
+    }
+
+    private static String floorReportResultTitle(Message message) {
+        return MESSTYPE.NOTICE.getType() +
+                "您对楼层" +
+                "" +   // TODO 获取楼层内容
+                "的举报" +
+                message.getPara().split(";")[1];
+    }
+
+    private static String commentReportResultTitle(Message message) {
+        return MESSTYPE.NOTICE.getType() +
+                "您对评论" +
+                "" +   // TODO 获取评论内容
+                "的举报" +
+                message.getPara().split(";")[1];
     }
 }
