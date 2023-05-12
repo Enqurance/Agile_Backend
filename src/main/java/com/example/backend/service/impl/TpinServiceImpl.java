de0315c1a1348d82bc0384beab7e3efca3bd0e19
WAN-M <1437207980@qq.com>
2023-05-12 11:50:57 +0800

    [feat](): tpin service

--- src/main/java/com/example/backend/service/impl/TpinServiceImpl.java
+++ src/main/java/com/example/backend/service/impl/TpinServiceImpl.java
@@ -4,17 +4,42 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.backend.domain.Tpin;
 import com.example.backend.service.TpinService;
 import com.example.backend.mapper.TpinMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author DELL
-* @description 针对表【tpin】的数据库操作Service实现
-* @createDate 2023-05-12 10:50:37
-*/
+ * @author DELL
+ * @description 针对表【tpin】的数据库操作Service实现
+ * @createDate 2023-05-12 10:50:37
+ */
 @Service
 public class TpinServiceImpl extends ServiceImpl<TpinMapper, Tpin>
-    implements TpinService{
+        implements TpinService {
+    @Autowired
+    private TpinMapper tpinMapper;
 
+    @Override
+    public int insertTask(int p_id) {
+        if (tpinMapper.findAllByPId(p_id).size() != 0) {
+            throw new RuntimeException("地图钉已提交过公开申请，请等待审核");
+        }
+
+        Tpin tpin = new Tpin();
+        tpin.setPId(p_id);
+        return tpinMapper.insertAll(tpin);
+    }
+
+    @Override
+    public int deleteTask(int id) {
+        return tpinMapper.deleteById(id);
+    }
+
+    @Override
+    public List<Tpin> findAllTasks() {
+        return tpinMapper.findAll();
+    }
 }
 
 
