448b5018c0bded1c1bfc294b5e071f1bd0b2d117
WAN-M <1437207980@qq.com>
2023-06-03 16:19:24 +0800

    [feat](): test http

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -24,41 +24,41 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Value("${http.port}")
-	Integer httpPort;
-
-	@Value("${server.port}")
-	Integer httpsPort;
-
-	@Bean
-	@ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint constraint = new SecurityConstraint();
-				constraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				constraint.addCollection(collection);
-				context.addConstraint(constraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(httpConnector());
-		return tomcat;
-	}
-
-	@Bean
-	@ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
-	public Connector httpConnector() {
-		System.out.println("启用http转https协议，http端口：" + this.httpPort + "，https端口：" + this.httpsPort);
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		//Connector监听的http的端口号
-		connector.setPort(httpPort);
-		connector.setSecure(false);
-		//监听到http的端口号后转向到的https的端口号
-		connector.setRedirectPort(httpsPort);
-		return connector;
-	}
+//	@Value("${http.port}")
+//	Integer httpPort;
+//
+//	@Value("${server.port}")
+//	Integer httpsPort;
+//
+//	@Bean
+//	@ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
+//	public TomcatServletWebServerFactory servletContainer() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				SecurityConstraint constraint = new SecurityConstraint();
+//				constraint.setUserConstraint("CONFIDENTIAL");
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				constraint.addCollection(collection);
+//				context.addConstraint(constraint);
+//			}
+//		};
+//		tomcat.addAdditionalTomcatConnectors(httpConnector());
+//		return tomcat;
+//	}
+//
+//	@Bean
+//	@ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
+//	public Connector httpConnector() {
+//		System.out.println("启用http转https协议，http端口：" + this.httpPort + "，https端口：" + this.httpsPort);
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setScheme("http");
+//		//Connector监听的http的端口号
+//		connector.setPort(httpPort);
+//		connector.setSecure(false);
+//		//监听到http的端口号后转向到的https的端口号
+//		connector.setRedirectPort(httpsPort);
+//		return connector;
+//	}
 }
